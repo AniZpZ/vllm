840403fe94c40a080ced14c72ae37b991bf79745
zhangying169 <zhangying169@meituan.com>
2023-10-12 19:09:53 +0800

    eval mmlu benchmark

---
+++ benchmarks/benchmark_evaluation.py
@@ -0,0 +1,239 @@
+import argparse
+# import asyncio
+# import json
+import os
+# import random
+# import time
+from typing import List, Tuple, Dict
+
+# import aiohttp
+import numpy as np
+import pandas as pd
+# from transformers import PreTrainedTokenizerBase
+# from vllm.transformers_utils.tokenizer import get_tokenizer
+from vllm import LLM, SamplingParams, RequestOutput
+from mmlu_template import MMLUTemplate
+import json
+
+TEMPLATE_REGITRY = {
+    "mmlu": MMLUTemplate,
+}
+
+
+def sample_requests(
+    # dataset_path: str,
+    # num_requests: int,
+    # tokenizer: PreTrainedTokenizerBase,
+    dev_data_path: str,
+    test_data_path: str,
+    dataset_template: str = "mmlu",
+    is_analyse: bool = False,
+    origin: bool = True,
+    subjects: List[str] = None
+) -> Tuple[List[str], List[str], List[int]]:
+    # Load the dataset.
+    nums_questions = []
+    dataset = []
+    labels = []
+    template_class = TEMPLATE_REGITRY[dataset_template]
+    for subject in subjects:
+        test_dataset = pd.read_csv(os.path.join(test_data_path, subject + "_test.csv"), header=None)
+        nums_questions.append(len(test_dataset))
+        template = template_class(subject, os.path.join(dev_data_path, subject + "_dev.csv"), is_analyse, origin)
+        for idx in range(len(test_dataset)):
+            prompt = template.getTemplate(test_dataset, idx)
+            dataset.append(prompt)
+            labels.append(test_dataset.iloc[idx, -1])
+    return dataset, labels, nums_questions
+
+
+def run_vllm(
+    requests: List[str],
+    output_len: int,
+    model: str,
+    tokenizer: str,
+    kv_cache_dtype: str = "int8",
+    kv_quant_params_path: str = None,
+    tensor_parallel_size: int = 1,
+    seed: int = 0,
+    n: int = 1,
+    use_beam_search: bool = False,
+    trust_remote_code: bool = False,
+    quantmethod: str = None,
+) -> List[RequestOutput]:
+    llm = LLM(
+        model=model,
+        tokenizer=tokenizer,
+        tensor_parallel_size=tensor_parallel_size,
+        seed=seed,
+        trust_remote_code=trust_remote_code,
+        kv_cache_dtype=kv_cache_dtype,
+        kv_quant_params_path=kv_quant_params_path,
+        quantization = quantmethod
+    )
+    for prompt in requests:
+        sampling_params = SamplingParams(
+            n=n,
+            temperature=0.0 if use_beam_search else 0.1,
+            top_p=1.0,
+            use_beam_search=use_beam_search,
+            ignore_eos=True,
+            max_tokens=output_len,
+        )
+        # FIXME(woosuk): Do not use internal method.
+        llm._add_request(
+            prompt=prompt,
+            prompt_token_ids=None,
+            sampling_params=sampling_params,
+        )
+    tokenizer = llm.get_tokenizer()
+    options = [tokenizer("A").input_ids[-1],
+                tokenizer("B").input_ids[-1],
+                tokenizer("C").input_ids[-1],
+                tokenizer("D").input_ids[-1]]
+    # FIXME(woosuk): Do use internal method.
+    return llm._run_engine(use_tqdm=True), options
+
+def mmlu_eval(probs, options):
+    probs = probs[options].detach().cpu().numpy()
+    pred = {0: "A", 1: "B", 2: "C", 3: "D"}[np.argmax(probs)]
+    return pred
+
+
+def evalute(
+    request_outputs: List[RequestOutput],
+    labels: List[str],
+    nums_questions: List[int],
+    subjects: List[str],
+    # dataset_template: str = "mmlu",
+    options = None
+) -> Dict[str, float]:
+    # template_class = TEMPLATE_REGITRY[dataset_template]
+    # pred = [template_class.findAnswer(r.outputs[0].text) for r in request_outputs]
+    pred = [mmlu_eval(r.outputs[0].probs, options) for r in request_outputs]
+    # pred = [template_class.findAnwerUsingRule(r.outputs[0].text) for r in request_outputs]
+    ids = np.cumsum(nums_questions)
+    lhs = 0
+    accs: List[float] = []
+    for rhs in ids:
+        pred_paritition = np.array(pred[lhs: rhs])
+        labels_partition = np.array(labels[lhs: rhs])
+        acc = np.mean(pred_paritition == labels_partition)
+        accs.append(acc)
+    sub2acc = {sub: acc for sub, acc in zip(subjects, accs)}
+    mmlu_category_path = "benchmarks/mmlu_category.json"
+    with open(mmlu_category_path, 'r', encoding='utf-8') as f:
+        sub_category = json.load(f)
+    cat_avg_acc = {}
+    for key, value in sub_category.items():
+        acc = 0.0
+        count = 0
+        for v in value:
+            if v in sub2acc.keys():
+                acc += sub2acc[v]
+                count += 1
+        if count > 0:
+            cat_avg_acc[key] = acc / count
+    return sub2acc, cat_avg_acc
+
+
+def main(args: argparse.Namespace):
+    # subjects = [
+    #     "abstract_algebra",
+    #     "anatomy",
+    #     "astronomy",
+    #     "college_biology",
+    #     "college_chemistry",
+    #     "college_computer_science",
+    #     "college_mathematics",
+    #     "college_physics",
+    #     "computer_security",
+    #     "conceptual_physics",
+    #     "electrical_engineering",
+    #     "elementary_mathematics",
+    #     "high_school_biology",
+    #     "high_school_chemistry",
+    #     "high_school_computer_science",
+    #     "high_school_mathematics",
+    #     "high_school_physics",
+    #     "high_school_statistics",
+    #     "machine_learning"
+    # ]
+    mmlu_category_path = "benchmarks/mmlu_category.json"
+    with open(mmlu_category_path, 'r', encoding='utf-8') as f:
+        sub_category = json.load(f)
+    # subjects = sub_category["Social Sciences"]
+    subjects = sub_category["Social Sciences"]
+    # subjects = sorted([f.split("_test.csv")[0] for f in os.listdir(args.test_data_path) if "_test.csv" in f])
+    dataset, labels, nums_questions = sample_requests(
+        args.dev_data_path,
+        args.test_data_path,
+        is_analyse=args.is_analyse,
+        origin=args.origin,
+        subjects=subjects
+    )
+    request_outputs, options = run_vllm(
+        dataset,
+        args.output_len,
+        args.model,
+        args.tokenizer,
+        args.kv_cache_dtype,
+        args.kv_quant_params_path,
+        args.tensor_parallel_size,
+        args.seed, args.n,
+        args.use_beam_search,
+        args.trust_remote_code,
+        args.quantization
+    )
+    sub2acc, cat_avg_acc = evalute(
+        request_outputs,
+        labels,
+        nums_questions,
+        subjects,
+        options
+        )
+    print(sub2acc)
+    print(cat_avg_acc)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="evaluation for quantization.")
+
+    parser.add_argument("--model", type=str, default="facebook/opt-125m")
+    parser.add_argument("--tokenizer", type=str, default=None)
+    parser.add_argument("--tensor-parallel-size", "-tp", type=int, default=1)
+    parser.add_argument("--n",
+                        type=int,
+                        default=1,
+                        help="Number of generated sequences per prompt.")
+    parser.add_argument("--use-beam-search", action="store_true")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument('--trust-remote-code',
+                        action='store_true',
+                        help='trust remote code from huggingface')
+    parser.add_argument("--dev-data-path",
+                        type=str,
+                        default=None,
+                        help="path to few-shot dataset")
+    parser.add_argument("--test-data-path",
+                        type=str,
+                        default=None,
+                        help="path to test dataset")
+    parser.add_argument("--is-analyse",
+                        action="store_true")
+    parser.add_argument("--output-len",
+                        type=int,
+                        default=100,
+                        help="nums of max token for evaluation outputs")
+    parser.add_argument("--origin", type=bool, default=True)
+    parser.add_argument("--kv-cache-dtype",
+                        type=str,
+                        default="float16")
+    parser.add_argument("--kv-quant-params-path",
+                        type=str,
+                        default=None)
+    parser.add_argument("--quantization",
+                        type=str,
+                        default=None)
+    args = parser.parse_args()
+    main(args)
---
+++ benchmarks/mmlu_template.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import json
+import os
+from langchain.prompts import PromptTemplate
+import openai
+import random
+
+template = PromptTemplate(
+            input_variables=["question", "A", "B", "C", "D", "Answer"],
+            template=
+"""
+USER: Question: {question}
+A. {A}
+B. {B}
+C. {C}
+D. {D} ASSISTANT: Answer: {Answer}
+""",
+        )
+
+template_origin = PromptTemplate(
+            input_variables=["question", "A", "B", "C", "D", "Answer"],
+            template=
+"""
+Question: {question}
+A. {A}
+B. {B}
+C. {C}
+D. {D}
+Answer: {Answer}
+""",
+        )
+
+
+template_with_analyse=PromptTemplate(
+            input_variables=["question", "A", "B", "C", "D"],
+            template=
+"""
+Q:{question}
+(A) {A} (B) {B} (C) {C} (D) {D}
+A: Let's think step by step.
+""",
+        )
+
+class MMLUTemplate():
+
+    def __init__(self, subject, file_path, is_analyse, origin):
+        self.fiveShotTemplate = ""
+        self.file_path = file_path
+        self.subject = subject
+        self.choices = ["A", "B", "C", "D"]
+        self.is_analyse = is_analyse
+        self.origin = origin
+        if not is_analyse:
+            if self.origin :
+                self.getFewShotBaseTemplates_origin()
+            else :
+                self.getFewShotBaseTemplates()
+        else:
+            self.getFewShotBaseTemplateAnalyse()
+
+    #########few_shot模板不带分析######################
+    def getFewShotBaseTemplates(self, k=2):
+        dev_df = pd.read_csv(self.file_path, header=None)
+
+        self.fewShotTemplate = self.gen_prompt(dev_df,self.subject, k)
+        return self.fewShotTemplate
+
+    def gen_prompt(self,train_df, subject, k=1):
+        prompt = "SYSTEM: The following are multiple choice questions (with answers) about {},Please select the correct answer from the options.".format(
+            subject.replace('_',' '))
+
+
+        for i in range(k):
+
+            prompt += template.format(question=train_df.iloc[i, 0],A=train_df.iloc[i, 1],B=train_df.iloc[i, 2],C=train_df.iloc[i, 3],
+                                      D=train_df.iloc[i, 4],Answer=train_df.iloc[i, 5] + "</s>")[1:-1]
+        return prompt
+
+    #########few_shot模板不带分析######################
+    def getFewShotBaseTemplates_origin(self, k=5):
+        dev_df = pd.read_csv(self.file_path, header=None)
+
+        self.fewShotTemplate = self.gen_prompt_origin(dev_df,self.subject, k)
+        return self.fewShotTemplate
+
+    def gen_prompt_origin(self,train_df, subject, k=1):
+        prompt = "The following are multiple choice questions (with answers) about {},Please select the correct answer from the options.\n".format(
+            subject.replace('_',' '))
+
+
+        for i in range(k):
+
+            prompt += template_origin.format(question=train_df.iloc[i, 0],A=train_df.iloc[i, 1],B=train_df.iloc[i, 2],C=train_df.iloc[i, 3],
+                                      D=train_df.iloc[i, 4],Answer=train_df.iloc[i, 5] + "\n")[1:-1]
+        return prompt
+
+
+
+
+    ###############################################################3############
+
+
+
+
+    ###########few_shot模板带分析，更改json文件就行###############################
+    def getFewShotBaseTemplateAnalyse(self):
+        mmlu_prompt = json.load(open('templates/lib_prompt/mmlu-cot.json'))
+        self.fewShotTemplate=mmlu_prompt[self.subject]
+        return self.fewShotTemplate
+
+
+
+    #################获得模板############
+    def getTemplate(self,test_df,i):
+        if self.is_analyse==True:
+            templ=template_with_analyse.format(question=test_df.iloc[i, 0],A=test_df.iloc[i, 1],B=test_df.iloc[i, 2],
+                                         C=test_df.iloc[i, 3],D=test_df.iloc[i, 4])
+
+            return self.fewShotTemplate + "\n" + templ
+
+        else:
+            if self.origin == True :
+                    prompt_end = template_origin.format(question=test_df.iloc[i, 0],A=test_df.iloc[i, 1],B=test_df.iloc[i, 2],
+                                                 C=test_df.iloc[i, 3],D=test_df.iloc[i, 4],Answer='')[1:-1]
+            else:
+                    prompt_end = template.format(question=test_df.iloc[i, 0],A=test_df.iloc[i, 1],B=test_df.iloc[i, 2],
+                                         C=test_df.iloc[i, 3],D=test_df.iloc[i, 4],Answer='')[1:-1]
+            return self.fewShotTemplate + prompt_end
+
+
+
+################################################解析函数##################################################################
+    def findAnswer(self,res):
+        # print("模型输出为:", res)
+        d = "NO"
+        for d_ in res:
+            if (ord(d_) >= 65 and ord(d_) <= 68):
+                d = d_
+                break
+        # if d == 'NO' :
+        #     d = random.choice(['A', 'B', 'C', 'D'])
+        # print("答案解析为:", d)
+        return d
+
+    def findAnwerUsingRule(self,res):
+        #print("模型输出为:", res)
+        result = "NO"
+        pattern = 'the answer is ('
+        try:
+
+            pred = res.lower().split(pattern)[1][0]
+
+            if (ord(pred.upper()) >= 65 and ord(pred.upper()) <= 68):
+                result = pred.upper()
+        except:
+            pass
+
+        #print("答案解析为:",result)
+        return result
+
+
+if __name__ == "__main__":
+    print(111)
+    BT=mmlu_template("abstract_algebra",os.path.join('../Imsys/MMLU/data', "dev", "abstract_algebra" + "_dev.csv"),is_analyse=False)
+
+
+    df=pd.read_csv(os.path.join('../Imsys/MMLU/data', "test", "abstract_algebra" + "_test.csv"), header=None)
+
+    temp=BT.getTemplate(df,3)
+    print('template==',temp)
